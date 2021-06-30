16470bf52e730e5f43f1e64fb6dc768c1355ecb8
Nellie Crocker <crocker.nellie@gmail.com>
2021-06-30 17:52:42 -0400

    finished movie app

--- src/components/apps/the-movie-db/MovieAppDisplay.js
+++ src/components/apps/the-movie-db/MovieAppDisplay.js
@@ -1,13 +1,48 @@
 import React from 'react';
+import { withRouter } from 'react-router';
+import { CarouselCaption } from 'reactstrap';
+import styled from 'styled-components';
 
-const MovieAppDisplay=({movie}) => {
+const Movie = styled.div`
+margin: 5px;
+width: 300px;
+height: 500px;
+border-radius: 5px;
+text-align: center;
+background: #48b1bf;
+`;
+
+const Poster = styled.img`
+width: calc(100% - 20px);
+height: calc(70% - 10px);
+margin: 10px 10px 0px 10px;
+border-radius: 5px;
+`;
+
+const Title = styled.h5`
+margin: 5px 0 0 0;
+color: white;
+`;
+
+const Description = styled.p`
+height: 20%;
+color: white;
+overflow-wrap: break-word;
+overflow: scroll;
+`;
+
+
+const MovieAppDisplay = ({ movie }) => {
     return (
-        <div>
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
-            <h5>{movie.original_title}</h5>
-            <p>{movie.overview}</p>
-        </div>
+        <Movie>
+            <Poster src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            <Title>{movie.original_title}</Title>
+            <Description>{movie.overview}</Description>
+        </Movie>
     );
 }
+
+
+
 
 export default MovieAppDisplay;
